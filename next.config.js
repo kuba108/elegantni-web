c7c391ce7441f9b8a5a9fa21cf60ff48168ef0e9
Kuba Malina <kuba@Kubas-MacBook-Air.local>
2025-11-23 10:49:38 +0100

    Fixes issue with pg

--- next.config.js
+++ next.config.js
@@ -8,6 +8,9 @@ const nextConfig = {
       },
     ],
   },
+  experimental: {
+    serverComponentsExternalPackages: ["sequelize", "pg", "pg-hstore", "bcryptjs"],
+  },
 };
 
 export default nextConfig;
